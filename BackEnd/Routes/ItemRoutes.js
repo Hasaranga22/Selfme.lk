3aa803463834603fc87545d3298a7fe0b92ec66a
Navoda Hasaranga <hasaranganavoda2025@gmail.com>
2025-09-01 17:59:07 +0530

    updated

--- BackEnd/Routes/ItemRoutes.js
+++ BackEnd/Routes/ItemRoutes.js
@@ -7,7 +7,7 @@ const path = require("path");
 // Configure storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../Item_images")); // save in Item_images
+    cb(null, path.join(__dirname, "../Item_images"));
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
@@ -22,7 +22,13 @@ router.get("/", itemController.getAllItems);
 // POST a new item with image upload
 router.post("/", upload.single("item_image"), itemController.createItem);
 
+// GET single item
 router.get("/:id", itemController.getItemById);
+
+// UPDATE item with potential image upload
+router.put("/:id", upload.single("item_image"), itemController.updateItem); // Added upload middleware
+
+// DELETE item
 router.delete("/:id", itemController.deleteItem);
 
 module.exports = router;
